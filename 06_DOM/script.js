6628e36e6ca23fed6471a899c00846c4becf12b9
Gaurav Yadav <gauravkumar.006382@gmail.com>
2026-08-11 10:28:55 +0530

    Added script

---
+++ 06_DOM/script.js
@@ -0,0 +1,6 @@
+document.getElementById('title').setAttribute('class' ,"test")
+document.getElementById('title').setAttribute('class' ,"test heading")
+const title = document.getElementById('title')
+title.style.backgroundColor = "white"
+title.style.backgroundColor = "blue"
+title.style.backgroundColor = "chocolate"
